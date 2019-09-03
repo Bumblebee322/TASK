3e7076ce7539daadd039e130902f3be74562a37a
Bumblebee322 <Shmelev507@gmail.com>
2019-09-03 09:59:20 +0300

    username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable,
          :omniauthable
-
+ # validates :username, presence: true, uniqueness: false
+  validates :email, presence: true, uniqueness: true
+  
 end
