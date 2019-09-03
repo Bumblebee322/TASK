316308a656c5f0c1a8cce69affde650035697f58
Bumblebee322 <Shmelev507@gmail.com>
2019-09-03 10:00:17 +0300

    username1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,5 @@ class ApplicationController < ActionController::Base
  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :current_password])
-    #devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password) }
-   #devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :current_password) }
 end
 end
