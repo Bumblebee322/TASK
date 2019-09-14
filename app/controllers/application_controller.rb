971dd153528f40e5fda8f3d1b0c9ca244b6cce37
Bumblebee322 <Shmelev507@gmail.com>
2019-09-14 08:24:26 +0300

    'EN/RU'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
-    
+
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_locale
 
     protected
 
@@ -9,4 +10,12 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :current_password])
     end
 
+
+    def set_locale
+        I18n.locale = params[:locale] || I18n.default_locale
+    end
+    def default_url_options(options = {})
+        {locale: I18n.locale}.merge options
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
+  
+  
   resources :companies 
 
   devise_for :users, controllers: {
+
     sessions: 'users/sessions',
     companies: 'companies#index',
     omniauth_callbacks: 'users/omniauth'
-  }
-  root 'home#index'
 
-  #get '/donate', to: 'donate#index', as: 'donate'
- # get 'companies/donate', as: 'donate'
+  }
+
+  
+  root 'home#index'
   get 'persons/profile', as: 'user_root'
   get 'users/edit', as: 'users_edit_path' 
 
